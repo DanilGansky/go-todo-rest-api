d9b924640be441b4643e3b294885c1938b59230e
danikg <danya.gansky@gmail.com>
2020-11-01 18:03:37 +0200

    Update imports

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,18 +8,18 @@ import (
 	"github.com/danikg/go-todo-rest-api/internal/models"
 	"github.com/gorilla/mux"
 
-	taghttp "github.com/danikg/go-todo-rest-api/internal/tag/controller/http"
-	tagrepo "github.com/danikg/go-todo-rest-api/internal/tag/repository"
-	tagservice "github.com/danikg/go-todo-rest-api/internal/tag/service"
-	tihttp "github.com/danikg/go-todo-rest-api/internal/todoitem/controller/http"
-	tirepo "github.com/danikg/go-todo-rest-api/internal/todoitem/repository"
-	tiservice "github.com/danikg/go-todo-rest-api/internal/todoitem/service"
-	tlhttp "github.com/danikg/go-todo-rest-api/internal/todolist/controller/http"
-	tlrepo "github.com/danikg/go-todo-rest-api/internal/todolist/repository"
-	tlservice "github.com/danikg/go-todo-rest-api/internal/todolist/service"
-	userhttp "github.com/danikg/go-todo-rest-api/internal/user/controller/http"
-	userrepo "github.com/danikg/go-todo-rest-api/internal/user/repository"
-	userservice "github.com/danikg/go-todo-rest-api/internal/user/service"
+	tagHttp "github.com/danikg/go-todo-rest-api/internal/tag/controller/http"
+	tagRepo "github.com/danikg/go-todo-rest-api/internal/tag/repository"
+	tagService "github.com/danikg/go-todo-rest-api/internal/tag/service"
+	todoItemHttp "github.com/danikg/go-todo-rest-api/internal/todoitem/controller/http"
+	todoItemRepo "github.com/danikg/go-todo-rest-api/internal/todoitem/repository"
+	todoItemService "github.com/danikg/go-todo-rest-api/internal/todoitem/service"
+	todoListHttp "github.com/danikg/go-todo-rest-api/internal/todolist/controller/http"
+	todoListRepo "github.com/danikg/go-todo-rest-api/internal/todolist/repository"
+	todoListService "github.com/danikg/go-todo-rest-api/internal/todolist/service"
+	userHttp "github.com/danikg/go-todo-rest-api/internal/user/controller/http"
+	userRepo "github.com/danikg/go-todo-rest-api/internal/user/repository"
+	userService "github.com/danikg/go-todo-rest-api/internal/user/service"
 )
 
 // App ...
@@ -39,25 +39,25 @@ func (a *App) Run() {
 	db := models.GetDB(a.Config)
 	router := mux.NewRouter()
 
-	userRepo := userrepo.NewUserRepository(db)
-	userService := userservice.NewUserService(userRepo)
-	userController := userhttp.NewUserController(userService)
-	userhttp.SetupRoutes(router, userController)
+	userRepo := userRepo.NewUserRepository(db)
+	userService := userService.NewUserService(userRepo)
+	userController := userHttp.NewUserController(userService)
+	userHttp.SetupRoutes(router, userController)
 
-	todoListRepo := tlrepo.NewTodoListRepository(db)
-	todoListService := tlservice.NewTodoListService(userRepo, todoListRepo)
-	todoListController := tlhttp.NewTodoListController(todoListService)
-	tlhttp.SetupRoutes(router, todoListController)
+	todoListRepo := todoListRepo.NewTodoListRepository(db)
+	todoListService := todoListService.NewTodoListService(userRepo, todoListRepo)
+	todoListController := todoListHttp.NewTodoListController(todoListService)
+	todoListHttp.SetupRoutes(router, todoListController)
 
-	todoItemRepo := tirepo.NewTodoItemRepository(db)
-	todoItemService := tiservice.NewTodoItemService(todoItemRepo, todoListRepo)
-	todoItemController := tihttp.NewTodoItemController(todoItemService)
-	tihttp.SetupRoutes(router, todoItemController)
+	todoItemRepo := todoItemRepo.NewTodoItemRepository(db)
+	todoItemService := todoItemService.NewTodoItemService(todoItemRepo, todoListRepo)
+	todoItemController := todoItemHttp.NewTodoItemController(todoItemService)
+	todoItemHttp.SetupRoutes(router, todoItemController)
 
-	tagRepo := tagrepo.NewTagRepository(db)
-	tagService := tagservice.NewTagService(tagRepo, todoItemRepo)
-	tagController := taghttp.NewTagController(tagService)
-	taghttp.SetupRoutes(router, tagController)
+	tagRepo := tagRepo.NewTagRepository(db)
+	tagService := tagService.NewTagService(tagRepo, todoItemRepo)
+	tagController := tagHttp.NewTagController(tagService)
+	tagHttp.SetupRoutes(router, tagController)
 
 	addr := a.Config.AppHost + ":" + a.Config.AppPort
 	log.Printf("starting at %s...", addr)
